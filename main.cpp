deed87b5e4af09b38ef5275aece938b48343d615
Danny <montealegredanielandres@gmail.com>
2021-11-10 18:47:02 -0500

    clean main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,10 @@
 #pragma once 
-#include <iostream>
-#include <vector>
-#include <complex>
-#include "Polynomial.h"
-#include "DifferenceEquations.h"
 #include "DifferenceEquationSolver.h"
-
-template <typename T>
-void print(T x)
-{
-	for ( auto& val : x)
-	{
-		std::cout << val << std::endl;
-	}
-}
 
 int main()
 {
-
-	std::cout << "===================================================================================================================" << std::endl; 
-	DifferenceEquation diffEqtn{ std::vector<double>{1.0, 1.0}, std::vector<double>{1.0,1.0} };
-	DifferenceEquationSolver solver{ diffEqtn }; 
-	auto foo = diffEqtn.generate(20);
-	print(foo); 
-	std::cout << "===================================================================================================================" << std::endl;
-
+	DifferenceEquation diffEqtn{ std::vector<double>{1.0, -2.0, -1.0}, std::vector<double>{0.0, 1.0,2.0} };
+	DifferenceEquationSolver solver{ diffEqtn }; 	
 	solver.solve(); 
-	int x = 1; 
 	return 0; 
 }
