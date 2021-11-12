2f09a30a51868de47160f68fb6392248cba213fd
Danny Montealegre <montealegredanielandres@gmail.com>
2021-11-12 17:26:55 -0500

    check point

--- GaussElimination.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include <complex> 
-#include <iostream>
-
-std::vector<std::complex<double>> GaussElimination(std::vector < std::vector<std::complex<double>>> A); 
-std::vector<double> GaussElimination(std::vector< std::vector<double> > A);
-void testGaussianElimination();
--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #pragma once 
-#include "DifferenceEquationSolver.h"
+# include "Algebra/DifferenceEquationSolver.h"
+# include "TimeSeries/AR.h"
+# include "TimeSeries/DataAnalysis.h"
+# include "LinearAlgebra/Matrix.h"
+
+template <typename T>
+void print(T x)
+{
+	for (auto val : x)
+	{
+		std::cout << val << std::endl;
+	}
+}
 
 int main()
 {
-	DifferenceEquation diffEqtn{ std::vector<double>{1.0, -2.0, -1.0}, std::vector<double>{0.0, 1.0,2.0} };
-	DifferenceEquationSolver solver{ diffEqtn }; 	
-	solver.solve(); 
+	//DifferenceEquation diffEqtn{ std::vector<double>{1.0, -2.0, -1.0}, std::vector<double>{0.0, 1.0,2.0} };
+	//DifferenceEquationSolver solver{ diffEqtn }; 	
+	//solver.solve(); 
+	//double sigma = 2.0;
+	//double c = -.75;
+	//
+	//AR test1{ std::vector<double>{c},sigma };
+
+	//std::cout << test1.isStationary() << std::endl; 
+
+	std::vector<double> row1{ 1.0, 2.0 }; 
+	std::vector<double> row2{ 0.0, 1.0 }; 
+	Matrix mat{ std::vector<std::vector<double>> {row1, row2} };
+
+	print(solveMatrixEqtn(mat, std::vector<double>{2.0, 3.0}));
+
 	return 0; 
 }
